b5a685cca13cf3a7f50d54f2fd476f42041db5f8
solomonubom54-png <solomonubom54@gmail.com>
2026-01-22 17:05:09 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Biverway | Lot Size Calculator",
+    layout="centered"
+)
+
+st.title("Biverway | Lot Size Calculator")
+
+st.write("This app will calculate lot size based on the Biverway Trading System.")
