3b734115d8549a832f4c7cb4c5cbcd07822ca972
solomonubom54-png <solomonubom54@gmail.com>
2026-01-22 17:20:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,19 @@ st.set_page_config(
 st.title("Biverway | Lot Size Calculator")
 
 st.write("This app will calculate lot size based on the Biverway Trading System.")
+
+# Symbol selector
+symbol = st.selectbox(
+    "Select Symbol",
+    ["EURUSD", "GBPUSD", "USDCHF", "XAUUSD"]
+)
+
+# Entry Price input
+entry = st.number_input("Entry Price", format="%.5f")
+
+# Stop Loss input
+sl = st.number_input("Stop Loss", format="%.5f")
+
+# Risk Amount input
+risk = st.number_input("Risk Amount", min_value=1.0, format="%.2f")
+
